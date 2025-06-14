ee7afb9003bacbe515982495f560d2ebc041f3eb
pirorin91 <pirorin91@gmail.com>
2025-06-14 17:08:29 +0900

    Add favicon

--- public/defense_highlight.php
+++ public/defense_highlight.php
@@ -110,6 +110,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($players_info[$player_name]))
 <head>
     <meta charset="UTF-8">
     <title>守備ハイライト</title>
+    <link rel="icon" type="image/png" href="icon128.png">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
